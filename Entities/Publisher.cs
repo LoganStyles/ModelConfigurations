e5e89e1cdb5b174d5c91a3652e1aab1c67c5fefa
LoganStyles <emmaist23@gmail.com>
2022-07-06 12:57:47 +0100

    feat(entities): added Publisher entity

---
+++ Entities/Publisher.cs
@@ -0,0 +1,9 @@
+namespace ModelConfigurations.Entities
+{
+    public class Publisher
+    {
+        public long Id { get; set; }
+        public string UserName { get; set; }
+        public string Address { get; set; }
+    }
+}
