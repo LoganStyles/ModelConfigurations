8e8580aaf75b922ff115e3e8631469596aa11461
LoganStyles <emmaist23@gmail.com>
2022-07-06 13:18:41 +0100

    feat(db context):added ArtistsContext class that extends dbcontext

---
+++ Data/ArtistsContext.cs
@@ -0,0 +1,27 @@
+using Microsoft.EntityFrameworkCore;
+using ModelConfigurations.Entities;
+
+namespace ModelConfigurations.Data
+{
+    public partial class ArtistsContext : DbContext
+    {
+        public ArtistsContext() { }
+
+        public ArtistsContext(DbContextOptions<ArtistsContext> options) : base(options) { }
+
+        public DbSet<Employee> Employees { get; set; }
+
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            modelBuilder.Entity<Publisher>();
+        }
+
+        protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+        {
+            if (!optionsBuilder.IsConfigured)
+            {
+                optionsBuilder.UseSqlite("data source=output/Artists.db");
+            }
+        }
+    }
+}
