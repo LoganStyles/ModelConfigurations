b754795b371ec60c80136171e5020f17cf0ddc53
LoganStyles <emmaist23@gmail.com>
2022-07-13 17:14:25 +0100

    feat(entity-properties): modified type properties to illustrate overriding conventions with data annotations

--- Entities/Event.cs
+++ Entities/Event.cs
@@ -1,10 +1,13 @@
+using System.ComponentModel.DataAnnotations;
 using Microsoft.EntityFrameworkCore;
 
 namespace ModelConfigurations.Entities
 {
     public class Event
     {
+        [Key]
         public long Id { get; set; }
+        public long ScheduleId { get; set; }
         public string Title { get; set; }
 
         [Comment("The date of the event")]
