57b8a024cb37dfbddd6a7af7605d19632bd5e137
LoganStyles <emmaist23@gmail.com>
2022-08-22 16:49:38 +0100

    feat(supplement conventions using data annotations): add required attribute to person.lastname

--- Entities/Person.cs
+++ Entities/Person.cs
@@ -1,10 +1,13 @@
+using System.ComponentModel.DataAnnotations;
+
 namespace ModelConfigurations.Entities
 {
     public class Person
     {
         public long Id { get; set; }
         public string FirstName { get; set; }
-        public string LastName { get; set; }
 
+        [Required]
+        public string LastName { get; set; }
     }
 }
